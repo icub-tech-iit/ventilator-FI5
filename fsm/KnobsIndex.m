ca591ee11839e3197d2b1128f3c6be0862ac8087
Ugo Pattacini <ugo.pattacini@iit.it>
2020-05-05 00:12:39 +0200

    [fsm] used model workspace to store data

--- fsm/KnobsIndex.m
+++
@@ -1,8 +0,0 @@
-classdef KnobsIndex < Simulink.IntEnumType
-  enumeration
-    BPM(0)
-    PIP(1)
-    IE_RATIO(2)
-    TV(3)
-  end
-end 
--- fsm/MapIndex.m
+++ fsm/MapIndex.m
@@ -1,7 +1,7 @@
 classdef MapIndex < Simulink.IntEnumType
   enumeration
-    Standard(1)
-    VCV(2)
-    Test(3)
+    MAP_Standard(1)
+    MAP_VCV(2)
+    MAP_Test(3)
   end
 end 
