34c0527abea55ee834a5590cb73d5933e0cd52c0
Ugo Pattacini <ugo.pattacini@iit.it>
2020-05-06 09:07:22 +0200

    [fsm] fix

--- fsm/MapIndex.m
+++ fsm/MapIndex.m
@@ -1,9 +1,10 @@
 classdef MapIndex < Simulink.IntEnumType
   enumeration
-    MAP_Standard(1)
-    MAP_VCV(2)
-    MAP_PRVC_1(3)
-    MAP_PRVC_2(4)
-    MAP_Test(5)
+    MAP_Transfer_None(0)
+    MAP_Transfer_Standard(1)
+    MAP_Transfer_VCV(2)
+    MAP_Transfer_PRVC_1(3)
+    MAP_Transfer_PRVC_2(4)
+    MAP_Transfer_Test(5)
   end
 end 
