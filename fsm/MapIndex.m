ab3a1135155fc1ed64f8adbc4f4f341be0f5d6fb
Ugo Pattacini <ugo.pattacini@iit.it>
2020-05-07 10:45:08 +0200

    [fsm] fixed map index

--- fsm/MapIndex.m
+++ fsm/MapIndex.m
@@ -5,6 +5,7 @@ classdef MapIndex < Simulink.IntEnumType
     MAP_Transfer_VCV(2)
     MAP_Transfer_PRVC_1(3)
     MAP_Transfer_PRVC_2(4)
-    MAP_Transfer_Test(5)
+    MAP_Transfer_AutoTrigger(5)
+    MAP_Transfer_Test(6)
   end
 end 
