ba95ceb7681a875ab4c010044b843cf5704f9dc4
Ugo Pattacini <ugo.pattacini@iit.it>
2020-08-22 14:17:42 +0200

    can tune TD correction gain

--- fsm/MapIndex.m
+++ fsm/MapIndex.m
@@ -5,7 +5,7 @@ classdef MapIndex < Simulink.IntEnumType
     MAP_Transfer_VCV(2)
     MAP_Transfer_PRVC_1(3)
     MAP_Transfer_PRVC_2(4)
-    MAP_Transfer_AutoTrigger(5)
+    MAP_Transfer_Advanced(5)
     MAP_Transfer_Test(6)
     MAP_ALARMS_1(7)
     MAP_ALARMS_2(8)
